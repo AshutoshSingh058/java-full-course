2e447e732eb0b44d4115d28c78002f60d591b7a4
AshutoshSingh058 <singhash058@gmail.com>
2026-08-20 14:48:36 +0530

    L 56

--- Java-Full-Course-main/Lecture 56/code/Demo3.java
+++ Java-Full-Course-main/Lecture 56/code/Demo3.java
@@ -4,8 +4,8 @@ import java.util.concurrent.Future;
 
 public class Demo3 {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-
+        // ExecutorService executor = Executors.newFixedThreadPool(2);
+// 
         // try {
         //     executor.execute(() -> {
         //         int x = 10 / 0;
@@ -15,15 +15,37 @@ public class Demo3 {
         //     System.out.println("Catched execute exception");
         // }
 
-        Future<Integer> f1 = executor.submit(() -> {
-            return (10 / 0);
-        });
+        // Future<Integer> f1 = executor.submit(() -> {
+        //     return (10 / 0);
+        // });
 
-        try {
-            System.out.println(f1.get());
+        // try {
+        //     System.out.println(f1.get());
+        // }
+        // catch(Exception e) {
+        //     System.out.println("Catched submit() exception");
+        // }
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        try{
+            executor.execute(()->{
+                int x = 10/0;
+            });
         }
-        catch(Exception e) {
-            System.out.println("Catched submit() exception");
+        catch(Exception e){
+            System.out.println("Catched execute exception");
         }
+
+        // Future<Integer> f1 = executor.submit(()->{
+        //     return (10/0);
+        // });
+
+        // try{
+        //     System.out.println(f1.get());
+        // }
+        // catch(Exception e) {
+        //     System.out.println("Catched submit exception");
+        // }
+
     }
 }
--- Java-Full-Course-main/Lecture 56/code/Demo4.java
+++ Java-Full-Course-main/Lecture 56/code/Demo4.java
@@ -1,36 +1,75 @@
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 import java.util.concurrent.TimeUnit;
 
 public class Demo4 {
    public static void main(String[] args) {
-        ThreadPoolExecutor executor = 
-        new ThreadPoolExecutor(2, 
-                              5,
-                              10 ,
-                              TimeUnit.SECONDS, 
-                              new ArrayBlockingQueue<>(2));
+        // ThreadPoolExecutor executor = 
+        // new ThreadPoolExecutor(2, 
+        //                       5,
+        //                       10 ,
+        //                       TimeUnit.SECONDS, 
+        //                       new ArrayBlockingQueue<>(2));
 
-        // ExecutorService executor2 = Executors.newFixedThreadPool(2);
+        // // ExecutorService executor2 = Executors.newFixedThreadPool(2);
 
         
-        for(int i = 1; i<=5; i++) {
+        // for(int i = 1; i<=5; i++) {
+        //     int taskId = i;
+
+        //     executor.execute(() -> {
+        //         System.out.println("Task " + taskId + " is perfromed by " +
+        //             Thread.currentThread().getName()
+        //         );
+
+        //         try {
+        //             Thread.sleep(2000);
+        //         }
+        //         catch(Exception e) {}
+        //     });
+        // }
+
+        // executor.shutdown();
+
+        // ExecutorService executor = Executors.newFixedThreadPool(3);
+        // ExecutorService executor = Executors.newCachedThreadPool();
+        // ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService executor = Executors.newScheduledThreadPool(2);
+        // ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        // executor.schedule(() -> {
+        //     System.out.println("Task executed after 3 seconds");
+        // }, 3, TimeUnit.SECONDS);
+        
+        for(int i=0; i<5; i++){
             int taskId = i;
-
-            executor.execute(() -> {
-                System.out.println("Task " + taskId + " is perfromed by " +
-                    Thread.currentThread().getName()
-                );
-
-                try {
-                    Thread.sleep(2000);
-                }
-                catch(Exception e) {}
+            executor.execute(()-> {
+                System.out.println("Task "+ taskId + " is performed by "+ Thread.currentThread()    .getName());
             });
         }
-
         executor.shutdown();
+
+        // ThreadPoolExecutor executor = new ThreadPoolExecutor(
+        //                                 2,
+        //                                 5, 
+        //                                 10, 
+        //                                 TimeUnit.SECONDS, 
+        //                                 new ArrayBlockingQueue<>(5) 
+        //                             ); 
+        
+        // for(int i=1; i<=5; i++){
+        //     int taskId = i;
+
+        //     executor.execute(()->{
+        //         System.out.println("Task "+ taskId+ " by "+ Thread.currentThread().getName());
+        //         try{
+        //             Thread.sleep(3000);
+        //         }
+        //         catch(Exception e){}
+        //     });
+
+        // }
+        // executor.shutdown();
    } 
 }
